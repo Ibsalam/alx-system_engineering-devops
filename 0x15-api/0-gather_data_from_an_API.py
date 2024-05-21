a186315bf0660025c80e1bf904cb03617f527912
ibsalam <asio4edu@gmail.com>
2024-05-20 22:25:59 -0700

    Gather data from API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,19 +2,29 @@
 '''
 Python script that, using this REST API, for a given employee ID
 '''
-import json
 import requests
+import json
 from sys import argv
 
 if __name__ == "__main__":
-    employee_id = sys.argv[1]
-    user = requests.get(url + "users/{}".format(employee_id)).json()
+    if len(argv) > 1:
+        employee_id = argv[1]
+        base_url = "https://jsonplaceholder.typicode.com"
+        user_url = f"{base_url}/users/{employee_id}"
+        todo_url = f"{base_url}/todos?userId={employee_id}"
 
-    params = {"userId": employee_id}
-    todos = requests.get(url + "todos", params).json()
+        user_response = requests.get(user_url)
+        user_data = user_response.json()
+        employee_name = user_data.get("name")
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
+        if employee_name is not None:
+            todo_response = requests.get(todo_url)
+            todo_data = todo_response.json()
 
-    print("Employee {} is done with tasks({}/{}):".format(user.get("name"), len(completed), len(todos)))
+            total_tasks = len(todo_data)
+            done_tasks = [task for task in todo_data if task["completed"]]
+            num_done_tasks = len(done_tasks)
 
-    [print("\t {}".format(complete)) for complete in completed]
+            print(f"Employee {employee_name} is done with tasks({num_done_tasks}/{total_tasks}):")
+            for task in done_tasks:
+                print(f"\t{task['title']}")
